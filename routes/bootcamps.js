8a0f0d951cfff2bf91119f4a7ab19bf8f6b57dff
Ridho Sirun Surinta <ridhosirunsurinta@gmail.com>
2021-02-18 15:24:54 +0900

    Refactoring routes

---
+++ routes/bootcamps.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+});
+
+router.get('/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Show bootcamps ${req.params.id}` });
+});
+
+router.post('', (req, res) => {
+  res.status(200).json({ success: true, msg: 'Create new bootcamps' });
+});
+
+router.put('/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Update bootcamps ${req.params.id}` });
+});
+
+router.delete('/:id', (req, res) => {
+  res.status(200).json({ success: true, msg: `Delete bootcamps ${req.params.id}` });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,30 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 // Load ENV vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-app.get('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
-});
-
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({ success: true, msg: `Show bootcamps ${req.params.id}` });
-});
-
-app.post('/api/v1/bootcamps', (req, res) => {
-  res.status(200).json({ success: true, msg: 'Create new bootcamps' });
-});
-
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({ success: true, msg: `Update bootcamps ${req.params.id}` });
-});
-
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-  res.status(200).json({ success: true, msg: `Delete bootcamps ${req.params.id}` });
-});
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
