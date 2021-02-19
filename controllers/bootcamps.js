777c31f4e06709cee2014ffbbb4892869209571b
Ridho Sirun Surinta <ridhosirunsurinta@gmail.com>
2021-02-19 15:04:00 +0900

    Add Create bootcamp functionality, Change database name & change location type & coordinates required to false

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -17,9 +17,17 @@ exports.getBootcamp = (req, res) => {
 // @desc    Create bootcamp
 // @route   POST /api/v1/bootcamps
 // @access  Private
-exports.createBootcamp = (req, res) => {
-  console.log(req.body);
-  res.status(200).json({ success: true, msg: 'Create new bootcamps' });
+exports.createBootcamp = async (req, res) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 }
 
 // @desc    Update bootcamp
