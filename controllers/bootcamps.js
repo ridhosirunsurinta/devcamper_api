ef0fe1ce0c42c75926de964295e889b00237ad6b
Ridho Sirun Surinta <ridhosirunsurinta@gmail.com>
2021-02-19 15:22:51 +0900

    Add Get All Bootcamps & Get Bootcamp by id

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -3,15 +3,27 @@ const Bootcamp = require('../models/Bootcamp');
 // @desc    Get All bootcamps
 // @route   GET /api/v1/bootcamps
 // @access  Public
-exports.getBootcamps = (req, res) => {
-  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
+exports.getBootcamps = async (req, res) => {
+  try {
+    const bootcamps = await Bootcamp.find();
+
+    res.status(200).json({ success: true, data: bootcamps });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 }
 
 // @desc    Get single bootcamp
 // @route   GET /api/v1/bootcamps/:id
 // @access  Public
-exports.getBootcamp = (req, res) => {
-  res.status(200).json({ success: true, msg: `Show bootcamp ${req.params.id}` });
+exports.getBootcamp = async (req, res) => {
+  try {
+    const bootcamp = await Bootcamp.findById(req.params.id);
+
+    res.status(200).json({ success: true, data: bootcamp });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 }
 
 // @desc    Create bootcamp
