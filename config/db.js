bb0152dd80c2d5f76af5c08498ca48188b2b20ac
Ridho Sirun Surinta <ridhosirunsurinta@gmail.com>
2021-02-22 11:52:12 +0900

    Add Database Seeder by calling node seeder -i & node seeder -d

--- config/db.js
+++ config/db.js
@@ -9,7 +9,7 @@ const connectDB = async () => {
     useUnifiedTopology: true,
   });
 
-  console.log(`MongoDB Connected: ${conn.connection.host}`.blue.underline);
+  console.log(`MongoDB Connected: ${conn.connection.host}`.blue.inverse);
 };
 
 module.exports = connectDB;
---
+++ seeder.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+// Load ENV variables
+dotenv.config({ path: './config/config.env' });
+
+// Load models
+const Bootcamp = require('./models/Bootcamp');
+
+// Connect to database
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+// Read JSON files
+const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8'));
+
+// Import into database
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+
+    console.log('Data imported'.green.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Delete data
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+
+    console.log('All data deleted'.red.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// node seeder -i
+if (process.argv[2] === '-i') {
+  importData();
+} else if (process.argv[2] === '-d') {
+  deleteData();
+}
