e019e891067e954df21f5e3bf30e5df7d9855a62
Ridho Sirun Surinta <ridhosirunsurinta@gmail.com>
2021-02-22 23:36:50 +0900

    Register func & Encrypt password

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,5 +6,15 @@ const asyncHandler = require('../middleware/async');
 // @route   POST /api/v1/auth/register
 // @access  Public
 exports.registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  });
+
   res.status(200).json({ success: true });
 });
