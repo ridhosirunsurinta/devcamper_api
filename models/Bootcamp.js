e42bbbea5ef6e4e25f0d6a7b5dff7aec96f706fd
Ridho Sirun Surinta <ridhosirunsurinta@gmail.com>
2021-02-22 11:35:02 +0900

    Implemented node-geocoder to Bootcamp model

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -40,13 +41,13 @@ const BootcampSchema = new mongoose.Schema({
   location: {
     // GeoJSON Point
     type: {
+      // Required
       type: String,
       enum: ['Point'],
-      required: false,
     },
     coordinates: {
+      // Required
       type: [Number],
-      required: false,
       index: '2dsphere',
     },
     formattedAddress: String,
@@ -104,6 +105,26 @@ const BootcampSchema = new mongoose.Schema({
 // Create Bootcamp slug from the name
 BootcampSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// Geocode & create location field
+BootcampSchema.pre('save', async function(next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].state,
+    zipcode: loc[0].zipcode,
+    country: loc[0].country,
+  };
+
+  // Do not save address in database
+  this.address = undefined;
+
   next();
 });
 
